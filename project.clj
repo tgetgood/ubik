a4c404a0b25ff30e5829584b25fccddd73758a54
Thomas Getgood <thomas.getgood@gmail.com>
2017-10-21 15:50:02 -0400

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,52 @@
+(defproject lemonade "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :min-lein-version "2.7.1"
+
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [org.clojure/spec.alpha "0.1.134"]]
+
+  :plugins [[lein-figwheel "0.5.13"]
+            [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
+
+  :source-paths ["src"]
+
+  :cljsbuild {:builds
+              [{:id "dev"
+                :source-paths ["src"]
+
+                :figwheel {:on-jsload "lemonade.core/on-js-reload"
+                           :open-urls ["http://localhost:3449/index.html"]}
+
+                :compiler {:main lemonade.core
+                           :asset-path "js/compiled/out"
+                           :output-to "resources/public/js/compiled/lemonade.js"
+                           :output-dir "resources/public/js/compiled/out"
+                           :parallel-build true
+                           :source-map-timestamp true
+                           :preloads [devtools.preload]}}
+               {:id "min"
+                :source-paths ["src"]
+                :compiler {:output-to "resources/public/js/compiled/lemonade.js"
+                           :main lemonade.core
+                           :optimizations :advanced
+                           :pretty-print false}}]}
+
+  :figwheel {:css-dirs ["resources/public/css"]}
+
+
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.4"]
+                                  [figwheel-sidecar "0.5.13"]
+                                  [com.cemerick/piggieback "0.2.2"]
+                                  [org.clojure/test.check "0.10.0-alpha2"]]
+                   ;; need to add dev source path here to get user.clj loaded
+                   :source-paths ["src" "dev"]
+
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+                   ;; need to add the compliled assets to the :clean-targets
+                   :clean-targets ^{:protect false} ["resources/public/js/compiled"
+                                                     :target-path]}})
