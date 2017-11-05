9d4c050b7de4f8fef0a5703ab209ef9187f4e9c1
Thomas Getgood <thomas.getgood@gmail.com>
2017-11-05 17:27:19 -0500

    Much cleaner draw loop.

--- src/lemonade/draw.cljs
+++ src/lemonade/draw.cljs
@@ -1,30 +1,26 @@
 (ns lemonade.draw
   "Event Loops")
 
-(defn draw! [render-fn context]
-  (if context
-    (render-fn context)
-    (render-fn)))
-
 (defn start-event-loop
   "Starts an event loop which draws the contents of an iref to context with
   renderer. Runs on requestAnimationFrame.
   Returns a function which when invoked kills the event loop."
-  [state-ref renderer & [context]]
+  [state-ref render draw]
   (let [last-state (atom nil)
-        counter (atom 0)
-        continue? (atom true)
-        frame (fn [state]
-                (when-not (= state @last-state)
-                  (reset! last-state state)
-                  (draw! (renderer state) context)))]
-    (letfn [(recurrent []
+        continue?  (atom true)]
+    (letfn [(recurrent [counter last-run]
               (js/window.requestAnimationFrame
-               (fn []
-                 (frame @state-ref)
+               (fn [now]
+                 (let [state @state-ref]
+                   (when-not (= state @last-state)
+                     (draw (render state))))
                  (when @continue?
-                   (js/window.requestAnimationFrame recurrent)))))]
-      (recurrent)
+                   (if (< 10000 (- now last-run))
+                     (do
+                       (println (* 1000 (/ counter (- now last-run))))
+                       (recurrent 0 now))
+                     (recurrent (inc counter) last-run))))))]
+      (recurrent 0 0)
 
       (fn []
         (reset! continue? false)))))
