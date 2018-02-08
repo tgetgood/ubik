52b2117f9768688559dde8bfcd9c3cbddc01def6
Thomas Getgood <thomas.getgood@gmail.com>
2018-02-08 16:45:55 -0500

    Version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject macroexpanse/lemonade "0.3.0"
+(defproject macroexpanse/lemonade "0.3.1-SNAPSHOT"
   :description "High level language for graphical and UI programming. No markup."
   :url "https://github.com/tgetgood/lemonade"
   :license {:name "Eclipse Public License"
