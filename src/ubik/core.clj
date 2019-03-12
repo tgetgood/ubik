3f4c344720ce1a84a9733f34eb5796a28797e136
Thomas Getgood <thomas.getgood@gmail.com>
2019-03-12 11:49:21 -0400

    Cleaning up dead code.

--- src/ubik/core.clj
+++ src/ubik/core.clj
@@ -8,10 +8,17 @@
             [ubik.rt :as rt])
   (:import javafx.scene.control.TextArea))
 
-(defn create-code-stage []
-  (let [p @(fx/code-stage)
-        ev-map (events/bind-text-area! (:area p))]
-    {:node (:area p) :stage (:stage p) :event-streams ev-map}))
+(defonce stages (atom {}))
+
+(defn create-code-stage [branch sym]
+  (let [k (str branch "-" sym)]
+    (if (contains? @stages k)
+      (@stages k)
+      (let [p @(fx/code-stage)
+            ev-map (events/bind-text-area! (:area p))
+            res {:node (:area p) :stage (:stage p) :event-streams ev-map}]
+        (swap! stages assoc k res)
+        res))))
 
 (defn lift [f]
   {:in (fn [_ x]
@@ -29,47 +36,3 @@
 (def image-signal codebase/image-signal)
 
 (def source-effector codebase/source-effector)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;; Aggregated API
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;;;; Subscriptions
-
-;; Distinction: subscriptions are reactive, signals are active. This is more
-;; important than it may seem.
-
-(defn wire [signal xform])
-
-(defn pipe [& xforms])
-
-(defmacro transducer
-  ([bindings body-map]
-   `(transducer {} ~bindings ~body-map))
-  ([opts bindings body-map]
-   (let [m (cond-> opts (= 2 (count bindings)) (assoc :state true))]
-     (into {} (map (fn [[k v]] [k `(with-meta (fn ~bindings ~v) ~m)])
-                   body-map)))))
-
-(defmacro deft [n & args]
-  (let [doc  (if (string? (first args)) (first args) nil)
-        args (if doc (rest args) args)]
-    `(def ~n
-       ~@(when doc [doc])
-       (transducer ~@args))))
-
-(defmacro subscription
-  "Given a form which derefs other subscriptions returns a new subscription
-  that reacts to its dependencies. If form does not depend on any subscriptions
-  then it is evaluated and it's (static) value returned."
-  [form]
-  `(subs/subscription ~form))
-
-;;; Processes
-
-(defmacro emit [& args]
-  `(process/emit ~@args))
-
-(def stateful-process process/stateful-process)
-
-(def subscription? subs/subscription?)
