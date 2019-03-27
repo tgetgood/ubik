647a5a309503fe8f81e2d51de59bc64574db40df
Thomas Getgood <thomas.getgood@gmail.com>
2019-03-26 21:49:03 -0400

    Back to where we were.

--- src/ubik/codeless.clj
+++ src/ubik/codeless.clj
@@ -26,7 +26,6 @@
 (def ns-ref
   (code/snippet {}
     (fn [image sym]
-      (println sym)
       (get-in image [(namespace sym) (name sym)]))))
 
 (def fn-snippet
@@ -46,17 +45,18 @@
 (def ns-lookup
   (code/snippet {ns-ref "8089627e22a94a6170547a184c5b4c7c99973c52"}
     (fn [image sym]
-      (invoke-by-id (:ref (ns-ref image sym))))))
+      (let [ref (ns-ref image sym)]
+        [(:name ref) (invoke-by-id (:ref ref))]))))
 
 (def fn-map
-  (code/snippet {ns-lookup "7f9311638e9c6839c11e7e8e14aa7aff3c4bd8ed"}
+  (code/snippet {ns-lookup "d30b4c5acbd03ac846e259970fcddf193b50dc81"}
     (fn [image syms]
       (into {} (map (partial ns-lookup image)) syms))))
 
 (def extract-deps
   "I've only named these as vars for the ease of reference"
   (code/snippet {fn-map {:ref :core/fn-map
-                         :time #inst "2019-03-27T01:04:54.563-00:00"}}
+                         :time #inst "2019-03-27T01:42:34.675-00:00"}}
     (fn [image]
       (fn-map image [:core/display
                      :core/format-code-text
@@ -132,7 +132,7 @@
   (code/snippet {edit-multi   {:ref  :core/edit-multi
                                :time #inst "2019-03-26T22:45:31.198-00:00"}
                  extract-deps {:ref  :core/extract-deps
-                               :time #inst "2019-03-27T01:05:23.348-00:00"}
+                               :time #inst "2019-03-27T01:42:51.180-00:00"}
                  topo-fac     {:ref  :core/snip-edit-topology
                                :time #inst "2019-03-26T22:45:31.198-00:00"}}
 
