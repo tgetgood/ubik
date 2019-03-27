4c40145be470d66e84429ca33b3daee824b3504a
Thomas Getgood <thomas.getgood@gmail.com>
2019-03-27 13:25:35 -0400

    File logging.
    Oh boy emacs seizes up quick.

--- src/ubik/util.clj
+++ src/ubik/util.clj
@@ -1,7 +1,37 @@
-(ns ubik.util)
+(ns ubik.util
+  (:require [clojure.pprint :refer [pprint]]
+            [taoensso.timbre :as log]))
 
 (defn vmap
   "Applies f to each value in map m, returning a map with the same keys, and
   transformed values."
   [f m]
   (into {} (map (fn [[k v]] [k (f v)])) m))
+
+(def logfile
+  "What do you think it is?"
+  "events.log")
+
+(def logfile-appender
+  {:enabled? true
+   :async? true
+   :min-level :debug
+   :rate-limit [[100 1000]]
+   :output-fn identity
+   :fn (fn [x]
+         (let [data (first (:vargs x))
+
+               meta (select-keys x [:instant :level :?ns-str :?line :?file])]
+
+           (spit logfile (str {:message data :metadata meta} "\n") :append true)))})
+
+(log/set-config!
+ {:level :debug
+  :timestamp-opts log/default-timestamp-opts
+  :middleware []
+  :output-fn identity
+  :appenders {:file-appender logfile-appender}})
+
+
+(defn log [level data]
+  (log/log level data))
